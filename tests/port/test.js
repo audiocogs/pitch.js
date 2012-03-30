b14fca11062a2964bedc05e8adcf0ec921830bcb
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-03-30 04:10:31 +0300

    Got the test working, as much as it can without the module working

--- tests/port/test.js
+++ tests/port/test.js
@@ -1,20 +1,37 @@
 #!/usr/bin/env node
 
 var debug = console.error.bind(console);
-var print = console.log.bind(console);
 
 var fs = require('fs');
+var Analyzer = require('../../src/pitch.js');
+var WavReader = require('../tools/wavreader.js');
+
+var BUFFER_LEN = 1024;
 
 function die (code) {
 	debug.apply(null, [].slice.call(arguments, 1));
 	process.exit(code || 0);
 }
 
-function fread (path) {
-	return fs.readFileSync(path, 'UTF-8');
+function format_float (n) {
+	n = String(n);
+	var i = n.indexOf('.');
+	return (i === -1 ? n + '.000000' : n.substring(0, i + 7)) + Array(i === -1 ? 1 : 7 - n.length + i).join('0');
 }
-function fwrite (path, data) {
-	return fs.writeFileSync(path, data, 'UTF-8');
+
+function process_data (data, analyzer, stream) {
+	stream.write(data.length + ' ');
+	if (data.length !== BUFFER_LEN) {
+		var ddata = new Float32Array(BUFFER_LEN);
+		ddata.set(data);
+		data = ddata;
+	}
+
+	analyzer.process(data);
+	var tone = analyzer.findTone();
+	var freq = tone ? tone.freq : 0.0;
+
+	stream.write(format_float(freq) + '\n');
 }
 
 var argv = [].slice.call(process.argv);
@@ -26,6 +43,38 @@ if (argv.length !== 4) {
 var inpath = argv[2];
 var outpath = argv[3];
 
-/* TODO: Actually do something here. For now, we'll just pretend */
 
-fwrite(outpath, 'TODO');
+debug("Generating test file `", outpath, "` from input data in `", inpath, "`");
+
+var infile, outfile;
+
+try {
+	infile = new WavReader(inpath);
+} catch (e) {
+	die(2, "Couldn't decode file `", inpath, "`");
+}
+
+if (infile.channelCount !== 1) {
+	die(3, "Input file `", inpath, "` is not mono (has", infile.channelCount, "channels)");
+}
+
+try {
+	outfile = fs.createWriteStream(outpath, {
+		encoding: 'UTF-8',
+		flags: 'w+'
+	});
+} catch (e) {
+	die(4, "Couldn't open file `", outpath, "` for writing");
+}
+
+var analyzer = new Analyzer(infile.sampleRate);
+
+var data;
+
+while (data = infile.readBuffer(BUFFER_LEN)) {
+	process_data(data, analyzer, outfile);
+}
+
+outfile.destroySoon();
+
+debug("Done");
